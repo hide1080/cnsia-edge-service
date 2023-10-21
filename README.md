7b57f370bfbe5107840608e308b2ac6fee880282
hide1080 <hide1080@gmail.com>
2023-10-21 23:35:08 +0900

    9章

---
+++ README.md
@@ -0,0 +1,2 @@
+# cnsia-edge-service
+「Cloud Native Spring in Action」の読書記録。エッジサービス
---
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,28 @@
+package com.polarbookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EdgeServiceApplicationTests {
+
+	private static final int REDIS_PORT = 6379;
+
+	@Container
+	static GenericContainer<?> redis =
+		new GenericContainer<>(DockerImageName.parse("redis:7.2.1"))
+			.withExposedPorts(REDIS_PORT);
+
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", () -> redis.getHost());
+		registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+	}
+
+	@Test
+	void verifyThatSpringContextLoads() {
+	}
+}
